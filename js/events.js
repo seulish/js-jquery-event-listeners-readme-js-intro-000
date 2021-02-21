f3c9d6e9c25950e8eab91a8ac33189f66ba328dc
Leah Lee <leah@iterable.com>
2021-02-21 00:27:58 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,48 @@
 //define functions here
+function getIt() {
+  $("p").on("click", function() {
+    window.alert("Hey!");
+  })
+}
+
+function frameIt() {
+  $("img").on("load", function() {
+    $("img").addClass("tasty");
+  })
+}
+
+function pressIt() {
+  $("#typing").on("keydown", function(key) {
+    if (key.which === 71) {
+      window.alert('G key was pressed');
+      return;
+    }
+    return;
+  })
+}
+
+//function pressIt() {
+//  $('#typing').on("keydown", function(press) {
+//    if (press.which === 71) {
+//      alert("You have entered g.");
+//    }
+//  })
+//}
+
+
+function submitIt() {
+  $("form").on("submit", function() {
+    window.alert('Your form is going to be submitted now.');
+    return;
+  }
+}
+
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 $(document).ready(function(){
-
 // call functions here
 
 });
